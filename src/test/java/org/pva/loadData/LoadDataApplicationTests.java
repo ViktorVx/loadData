aa0226d4a604da66da98dc8c9732585302f65ffc
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-10-26 19:11:21 +0300

    Init

---
+++ src/test/java/org/pva/loadData/LoadDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pva.loadData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

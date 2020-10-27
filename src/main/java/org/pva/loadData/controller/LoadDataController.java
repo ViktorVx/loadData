d0a7a44b68a7a8f060f593491712ecff569441b9
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-10-27 20:24:21 +0300

    Add creation of 20M rows file

--- src/main/java/org/pva/loadData/controller/LoadDataController.java
+++ src/main/java/org/pva/loadData/controller/LoadDataController.java
@@ -3,25 +3,35 @@ package org.pva.loadData.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("file")
 public class LoadDataController {
 
+    private static final Integer defaultRows = 5;
+
     @GetMapping(path = "download/{number}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity download(@PathVariable Integer number) {
-        String s = IntStream.range(1, number == null ? 5 : number + 1)
-                .mapToObj(i -> String.format("%s;Client%d;%d", UUID.randomUUID(), i, 100))
-                .collect(Collectors.joining("\n"));
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header("Content-disposition", "attachment; filename=clients.txt")
-                .body(s);
+    public ResponseEntity download(@PathVariable Integer number) throws IOException {
+        String fileName = UUID.randomUUID().toString().concat(".txt");
+
+        Files.write(Paths.get(fileName),
+                (Iterable<String>) IntStream.range(1, number == null ? defaultRows : number + 1)
+                        .mapToObj(i -> String.format("%s;Client%d;%d", UUID.randomUUID(), i, 100))::iterator
+                );
+        // todo create zip file in memory and remove file on disk
+        // todo try NOT to save txt file to the disk
+
+        return ResponseEntity.ok("ok ".concat(String.valueOf(number)));
     }
 
     @RequestMapping(path = "upload")

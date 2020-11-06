696349b3ce55d0bd379848123c7743fe413029f0
ViktorVx <petrov.v.anatolyev@sberbank.ru>
2020-11-06 18:02:29 +0300

    Complete

--- src/main/java/org/pva/loadData/controller/LoadDataController.java
+++ src/main/java/org/pva/loadData/controller/LoadDataController.java
@@ -1,31 +1,20 @@
 package org.pva.loadData.controller;
 
-import org.pva.loadData.model.Client;
-import org.pva.loadData.repository.ClientRepository;
 import org.pva.loadData.service.DownloadService;
+import org.pva.loadData.service.UploadService;
+import org.pva.loadData.utils.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("file")
 public class LoadDataController {
 
-    @Value("${loadData.buffer.size}")
-    private int bufferSize;
-
-    @Value("${loadData.rowsBuffer.size}")
-    private int rowsBufferSize;
-
     private DownloadService downloadService;
-    private ClientRepository clientRepository;
+    private UploadService uploadService;
 
     @GetMapping(path = "download/{number}", produces = "application/zip")
     public byte[] download(@PathVariable Integer number) {
@@ -34,71 +23,14 @@ public class LoadDataController {
 
     @PostMapping(path = "upload", consumes = "multipart/form-data")
     public String upload(@RequestParam("file") MultipartFile file) {
-        if (!file.isEmpty()) {
-            byte[] bytes;
-            try {
-                bytes = file.getBytes();
-            } catch (IOException e) {
-                return "Вам не удалось загрузить  => " + e.getMessage();
-            }
-
-            try(ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
-                ZipInputStream zipStream = new ZipInputStream(byteStream)) {
-
-                ZipEntry entry = zipStream.getNextEntry();
-
-                StringBuilder s = new StringBuilder();
-                //todo make this variables as parameters in properties
-                byte[] buffer = new byte[bufferSize];
-                int read;
-                List<String> rowsBuffer = new ArrayList<>();
-                while(entry != null) {
-                    while ((read = zipStream.read(buffer, 0, bufferSize)) >= 0) {
-                        s.append(new String(buffer, 0, read));
-                        String[] rows = s.toString().split("\n");
-
-                        List<String> cleanRows;
-                        if (s.toString().endsWith("\n")) {
-                            cleanRows = List.of(rows);
-                            s = new StringBuilder();
-                        } else {
-                            cleanRows = List.of(rows).subList(0, rows.length - 1);
-                            s = new StringBuilder(rows[rows.length - 1]);
-                        }
-
-                        rowsBuffer.addAll(cleanRows);
-                        //***
-                        if (rowsBuffer.size() >= rowsBufferSize) {
-                            persistRowsBuffer(rowsBuffer);
-                            rowsBuffer.clear();
-                        }
-                        //***
-                    }
-                    entry = zipStream.getNextEntry();
-                }
-                if (!rowsBuffer.isEmpty()) {
-                    persistRowsBuffer(rowsBuffer);
-                    rowsBuffer.clear();
-                }
-                return "Вы удачно загрузили!";
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Вам не удалось загрузить  => " + e.getMessage();
-            }
-        } else {
-            return "Вам не удалось загрузить потому что файл пустой.";
+        long start = System.nanoTime();
+        try {
+            uploadService.uploadData(file);
+        } catch (IOException e) {
+            return "File doesn't load: " + e.getMessage();
         }
-    }
-
-    private void persistRowsBuffer(List<String> rowsBuffer) {
-        List<Client> clients = new ArrayList<>();
-        for (String s : rowsBuffer) {
-            System.out.println("*** ".concat(s));
-            String[] parts = s.split(";");
-            clients.add(new Client(parts[0], parts[1], Long.valueOf(parts[2])));
-        }
-        clientRepository.batchUpdate(clients);
-        System.out.println("Persist-----------------");
+        long end = System.nanoTime();
+        return "Success load " + CommonUtils.nanosToMinSec(end - start);
     }
 
     @Autowired
@@ -107,7 +39,7 @@ public class LoadDataController {
     }
 
     @Autowired
-    public void setClientRepository(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
+    public void setUploadService(UploadService uploadService) {
+        this.uploadService = uploadService;
     }
 }
